1448474a3bc5bb19bbb84d9f61e5095f0c853184
faragelo <khaledfarghaly011@gmail.com>
2024-06-30 23:24:21 +0300

    Configure PostgreSQL database settings for the 'bookmarks' project

--- bookmarks/settings.py
+++ bookmarks/settings.py
@@ -77,8 +77,10 @@ WSGI_APPLICATION = 'bookmarks.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'bookmarks',
+        'USER': 'bookmarks',
+        'PASSWORD': '123',
     }
 }
 
