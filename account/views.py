1575560c23b8dfd404a2b017855f0fb55d0a00e5
faragelo <khaledfarghaly011@gmail.com>
2024-07-05 01:24:51 +0300

    Added Edit user profile

--- account/views.py
+++ account/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+
+from .models import Profile
 from .forms import UserRegistrationForm
 
 @login_required
@@ -14,6 +16,7 @@ def register(request):
             new_user = user_form.save(commit=False)
             new_user.set_password(user_form.cleaned_data['password'])
             new_user.save()
+            Profile.objects.create(user=new_user)
             return render(request, 'account/register_done.html', {'new_user': new_user})
     else:
         user_form = UserRegistrationForm()
